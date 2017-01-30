4595588916741f0c861cb10581198537626f71b5
Karl Falconer <karl.falconer@falconerdevelopment.com>
2017-01-30 15:41:07 -0500

    test local deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+CIRCLE_TAG=v0.3.0-rc7
 # Prepare git for pushing version bump to
 #git config --global push.default simple
 #git config --global user.name "Karl Falconer"
@@ -9,6 +9,11 @@
 #echo -e "---\n:host: http://x:${GEM_PASSWORD}@gems.falconerdevelopment.com" > ~/.gem/geminabox
 
 # release gem
-bundle exec bump set ${CIRCLE_TAG//v} --commit-message "version bump [skip ci]" 
-bundle exec rake release
+sed -i -e "s/VERSION = .*/VERSION = \"${CIRCLE_TAG//v}\"/" lib/hola/version.rb
+bundle
+git add --update lib/hola/version.rb
+git add --update Gemfile.lock
+git commit -m "version bump [skip ci]"
+#bump set ${CIRCLE_TAG//v} --commit-message "version bump [skip ci]" 
+#bundle exec rake release
 echo "Deployment complete"
